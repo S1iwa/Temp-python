ddca691c825474e89d604aa5db3af6c2554b917f
S1iwa <michalsliwa04@gmail.com>
2026-03-17 21:18:51 +0100

    Dodano 2 testy sprawdzające funkcje countMembers

--- tests/tests.py
+++ tests/tests.py
@@ -1,1 +1,14 @@
-import src.functions as fnc
+from src.functions import countMembers
+
+def main():
+    czlonkowieZero = []
+    czlonkowieJeden = ["Miłosz Przybył"]
+
+    print("Zero członków w zespole:")
+    print(countMembers(czlonkowieZero))
+
+    print("Jeden członek w zespole:")
+    print(countMembers(czlonkowieJeden))
+
+if __name__ == "__main__":
+    main()
